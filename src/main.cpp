adda1929042c4c53e423efb2751a086f63d5948d
Sarah Edwards <skedwards88@gmail.com>
2026-04-29 06:48:04 -0700

    fix a few bugs

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ const unsigned int LAYER_ON_TIME_US = 800;
 // (consider a line with between each of these coordinates and
 // the coordinate {0,0,0} representing the position).
 // todocolin--can omit some directions if desired
-const int DIRECTIONS[13][3] = {
+const int DIRECTIONS[][3] = {
     // Vary just x, y, or z
     {1, 0, 0},
     {0, 1, 0},
@@ -44,7 +44,8 @@ const int DIRECTIONS[13][3] = {
     {1, 1, 1},
     {1, -1, 1},
     {1, 1, -1},
-    {-1, 1, 1}};
+    {-1, 1, 1}
+  };
 
 struct CubeState
 {
@@ -71,66 +72,96 @@ int getNewState(const uint8_t cube[NUM_LAYERS][NUM_POSITIONS], int layer, int po
   int y = position / GRID_DIMENSION; // C++ automatically rounds down for integer division, so no need for something like Math.floor()
   uint8_t currentValue = getValueAtXYZ(cube, x, y, layer);
 
-  int numDeadNeighbors = 0;
-  int numAliveNeighbors = 0;
+  int numNeighborsWithState0 = 0;
+  int numNeighborsWithState1 = 0;
+  int numNeighborsWithState2 = 0;
+  int numNeighborsWithState3 = 0;
 
-  for (uint8_t d = 0; d < 13; d++)
+  for (const auto &d : DIRECTIONS)
   {
     // todo instead of storing 13 directions and doing math twice, could store the 26 neighbors and do math once. Or could consolidate the double mathing.
     // todo using all 26 neighbors, but could reduce
-    int dx = DIRECTIONS[d][0];
-    int dy = DIRECTIONS[d][1];
-    int dz = DIRECTIONS[d][2];
+    int dx = d[0];
+    int dy = d[1];
+    int dz = d[2];
 
     int neighbor1X = x + dx;
     int neighbor1Y = y + dy;
     int neighbor1Z = layer + dz;
 
-    uint8_t neighbor1Value = getValueAtXYZ(cube, neighbor1X, neighbor1Y, neighbor1Z);
+    bool neighbor1IsOffCube = neighbor1X < 0 || neighbor1Y < 0 || neighbor1Z < 0 || neighbor1X >= GRID_DIMENSION || neighbor1Y >= GRID_DIMENSION || neighbor1Z >= GRID_DIMENSION;
+    if (!neighbor1IsOffCube)
+    {
 
-    if (neighbor1Value == 0)
-    {
-      numDeadNeighbors++;
-    }
-    else
-    {
-      numAliveNeighbors++;
+      uint8_t neighbor1Value = getValueAtXYZ(cube, neighbor1X, neighbor1Y, neighbor1Z);
+
+      // todo can probably clean this up
+      if (neighbor1Value == 0)
+      {
+        numNeighborsWithState0++;
+      }
+      else if (neighbor1Value == 1)
+      {
+        numNeighborsWithState1++;
+      }
+      else if (neighbor1Value == 2)
+      {
+        numNeighborsWithState2++;
+      }
+      else if (neighbor1Value == 3)
+      {
+        numNeighborsWithState3++;
+      }
     }
 
     int neighbor2X = x - dx;
     int neighbor2Y = y - dy;
     int neighbor2Z = layer - dz;
 
-    uint8_t neighbor2Value = getValueAtXYZ(cube, neighbor2X, neighbor2Y, neighbor2Z);
+    bool neighbor2IsOffCube = neighbor2X < 0 || neighbor2Y < 0 || neighbor2Z < 0 || neighbor2X >= GRID_DIMENSION || neighbor2Y >= GRID_DIMENSION || neighbor2Z >= GRID_DIMENSION;
 
-    if (neighbor2Value == 0)
+    if (!neighbor2IsOffCube)
     {
-      numDeadNeighbors++;
-    }
-    else
-    {
-      numAliveNeighbors++;
+      uint8_t neighbor2Value = getValueAtXYZ(cube, neighbor2X, neighbor2Y, neighbor2Z);
+
+      if (neighbor2Value == 0)
+      {
+        numNeighborsWithState0++;
+      }
+      else if (neighbor2Value == 1)
+      {
+        numNeighborsWithState1++;
+      }
+      else if (neighbor2Value == 2)
+      {
+        numNeighborsWithState2++;
+      }
+      else if (neighbor2Value == 3)
+      {
+        numNeighborsWithState3++;
+      }
     }
   }
 
   // todocolin--can change the conditions for life here
-  // dead = 0, alive = 1. Not using the other colors, but they would be 2
-  // if currently dead: if has exactly 2 live neighbors -> becomes alive
-  // if currently alive: if has <2 live neighbors -> becomes dead
-  // if currently alive: if has >3 live neighbors -> becomes dead
-  // otherwise, unchanged
-  if (currentValue == 0 && numAliveNeighbors == 2)
+  // 0 = off, 1 = Color 1, 2 = Color 2, 3 = Color 1+2
+  //
+  // if currently 0: if has exactly 2 non-0 neighbors -> becomes 1
+  if (currentValue == 0 && (numNeighborsWithState1 + numNeighborsWithState2 + numNeighborsWithState3) == 2)
   {
     return 1;
   }
-  else if (currentValue == 1 && numAliveNeighbors < 2)
+  // if currently not 0: if has <2 non-0 neighbors -> becomes dead
+  else if (currentValue != 0 && (numNeighborsWithState1 + numNeighborsWithState2 + numNeighborsWithState3) < 2)
   {
     return 0;
   }
-  else if (currentValue == 1 && numAliveNeighbors > 3)
+  // if currently not 0: if has >3 non-0 neighbors -> becomes dead
+  else if (currentValue != 0 && (numNeighborsWithState1 + numNeighborsWithState2 + numNeighborsWithState3) > 3)
   {
     return 0;
   }
+  // otherwise, unchanged
   else
   {
     return currentValue;
@@ -201,6 +232,16 @@ void renderCube(CubeState &cubeState)
 
 void updateCube(CubeState &cubeState)
 {
+  // Make the updates based on the initial state, THEN update the cube state
+  uint8_t nextCube[NUM_LAYERS][NUM_POSITIONS];
+
+  for (int layer = 0; layer < NUM_LAYERS; layer++)
+  {
+    for (int position = 0; position < NUM_POSITIONS; position++)
+    {
+      nextCube[layer][position] = getNewState(cubeState.cube, layer, position);
+    }
+  }
 
   // todo do I need to track both the cube and cachedLayerBytes?
   for (int layer = 0; layer < NUM_LAYERS; layer++)
@@ -213,10 +254,10 @@ void updateCube(CubeState &cubeState)
 
     for (int position = 0; position < NUM_POSITIONS; position++)
     {
-      cubeState.cube[layer][position] = getNewState(cubeState.cube, layer, position);
+      cubeState.cube[layer][position] = nextCube[layer][position];
 
-      bool color1IsOn = cubeState.cube[layer][position] == 1 || cubeState.cube[layer][position] == 3;
-      bool color2IsOn = cubeState.cube[layer][position] == 2 || cubeState.cube[layer][position] == 3;
+      bool color1IsOn = nextCube[layer][position] == 1 || nextCube[layer][position] == 3;
+      bool color2IsOn = nextCube[layer][position] == 2 || nextCube[layer][position] == 3;
 
       setBytes(position, color1IsOn, color2IsOn, cubeState.cachedLayerBytes[layer]);
     }
@@ -244,7 +285,7 @@ void setup()
     for (int position = 0; position < NUM_POSITIONS; position++)
     {
       // todocolin--can change the initial state here. random() is min inclusive, max exclusive
-      cubeState.cube[layer][position] = random(0, 2); // todo can max be set to a variable?
+      cubeState.cube[layer][position] = random(0, 4); // todo can max be set to a variable?
     }
 
     // Just set the cached bytes to all off; it will update on the schedule
